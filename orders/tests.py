c7d9410791a8360f4ceda1a5151762c0844943b6
hala-hagag <halawaelhaggag@gmail.com>
2024-12-21 20:46:36 +0200

    Add test cases for users, orders, and products

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from orders.models import Order
+from users.models import CustomUser
 
-# Create your tests here.
+class OrderModelTest(TestCase):
+    def test_create_order(self):
+        user = CustomUser.objects.create_user(
+            username='testuser', email='test@example.com', password='testpassword'
+        )
+        order = Order.objects.create(user=user, shipping_address="123 Test St", billing_address="123 Test St", total_price=100.00)
+        self.assertEqual(order.user.username, 'testuser')
+        self.assertEqual(order.total_price, 100.00)
