49b8e07825510aac56f84805c52b7131fefa80f0
hala-hagag <halawaelhaggag@gmail.com>
2024-12-21 20:46:36 +0200

    Add test cases for users, orders, and products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from products.models import Product
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def test_create_product(self):
+        product = Product.objects.create(name="Test Product", price=25.99, description="A cool T-shirt")
+        self.assertEqual(product.name, "Test Product")
+        self.assertEqual(product.price, 25.99)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = get_user_model().objects.create_user(
+            username='testuser', email='test@example.com', password='testpassword'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('testpassword'))
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            username='admin', email='admin@example.com', password='adminpassword'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
