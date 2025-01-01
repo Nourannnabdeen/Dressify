9d87d694de58ea95c12aed1fe0ab692effbf9494
hala-hagag <halawaelhaggag@gmail.com>
2025-01-01 13:51:29 +0200

    Fix observer pattern integration and update apps.py

--- orders/apps.py
+++ orders/apps.py
@@ -4,10 +4,6 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
-    
-class YourAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'your_app'
 
     def ready(self):
-        import your_app.signals
+        import orders.signals  # Ensure signals are loaded when the app starts
